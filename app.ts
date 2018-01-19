96dab4fca50c8221c5ff9b32eb1c5aa4ac65370a
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-19 18:35:54 +0100

    +

--- app.ts
+++ app.ts
@@ -23,7 +23,59 @@ function returnAnyString(anyString: string):string{
 
 console.log(returnMyName());
 
-console.log(returnAnyString(true));
+console.log(returnAnyString("Vlada"));
+
+//function type === function expression
+
+function func1(value1: number, value2: string):boolean{
+    return true;
+}
+
+function func2(value1: string, value2: string):boolean{
+    return true;
+}
+
+let x:(val1:string, val2:string)=>boolean;
+
+//x=func1; //invalid assigment
+x=func2;
+
+//object types and values
+let myObj: { name: string, age: number} = { name: "Vladimir", age:40 };
+
+//create new types
+type Complex={a:number, b:number};
+
+let xDev:Complex={a:5, b:6};
+console.log(xDev);
+
+let myValue : string | number;
+
+myValue=5;
+myValue="Vladimir";
+//false
+//myValue=false;
+type ba = { money: number, deposit: (value:number)=>void };
+
+let bankAccount1 :ba  = {
+    money:2000,
+    deposit(value:number)
+    {
+        this.money += value;
+        console.log("money" + this.money);
+    }
+}
+
+let myself: { name:string, bankAccount: ba, hobbies: string[] } = {
+    name: "Max",
+    bankAccount: bankAccount1,
+    hobbies: ["Sports", "Cooking"]
+}
+
+myself.bankAccount.deposit(2000);
+console.log(myself);
+
+console.log("vlada");
 
 
 
