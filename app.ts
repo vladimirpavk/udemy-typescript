5fe63a4147c50cc0fc39a0efb3276923ca3e73b7
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-30 14:23:01 +0100

    ..end of classes

--- app.ts
+++ app.ts
@@ -64,3 +64,74 @@ class ITProject extends Project{
 let itproj1 = new ITProject();
 itproj1.changeName("New Name");
 console.log(itproj1.name);
+
+class StaticProba{
+    private static counter:number=0;
+    public incCounter():void{ StaticProba.counter++; }
+    public decCounter():void{ StaticProba.counter--; }
+    public getCounter():number{ return StaticProba.counter; }
+}
+
+let ps1:StaticProba=new StaticProba();
+let ps2:StaticProba=new StaticProba();
+
+ps1.incCounter();
+ps1.incCounter();
+console.log("From ps2: " + ps2.getCounter());
+
+class SingletonClass{
+    private static instance:SingletonClass;
+    private constructor(public name:string){ }
+    public static getInstance(){
+        if(!SingletonClass.instance) SingletonClass.instance=new SingletonClass("Vladimir");
+        return SingletonClass.instance;
+    }
+}
+
+let st1=SingletonClass.getInstance();
+
+//Module Exercise
+class Car{
+
+    public name:string;
+    public acceleration:number=0;
+
+    construtor(name:string){
+        this.name=name;
+    }
+
+    public accelerate(speed:number){
+        this.acceleration+=speed;
+    }
+
+    public honk(){
+        console.log("Toooooooooooot");
+    }
+
+}
+
+class Rectangle{
+    public width:number=0;
+    public length:number=0;
+
+    public calcSize():number{
+        return this.width*this.length;
+    }
+}
+
+class Persona{
+    private _firstName:string="";
+    set firstName(value:string){
+        if(value.length > 3){
+            this._firstName=value;
+        }
+        else
+        {
+            this._firstName="";
+        }
+    }
+    get firstName():string{
+        return this._firstName;
+    }
+
+}
