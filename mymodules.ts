feca03a962f996da8bc219ec3b8dab4153d63790
Vladimir Pavković <vladimirpavk@gmail.com>
2018-01-31 09:07:59 +0100

    +

---
+++ mymodules.ts
@@ -0,0 +1,13 @@
+export class Klasa1 {
+    constructor(){
+        console.log("Klasa1 konsstruktor");
+    }
+}
+
+export class Klasa2 {
+    constructor(){
+        console.log("Klasa2 konstruktor");
+    }
+}
+
+export const vreme:number = 5;
