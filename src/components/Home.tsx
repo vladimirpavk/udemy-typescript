999775ebcb1dd49f0365c2b5ae339fb8075bb4cd
Vladimir Pavković <vladimirpavk@gmail.com>
2018-02-23 09:42:06 +0100

    +

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export default class Home extends React.Component<HomeProps, {}>{
     render(){
         return(
             <div>
-                Hello there, {this.props.name}, you are {this.props.age}, right ?
+                Hellao there, {this.props.name}, you are {this.props.age}, right ?
             </div>
         )
     }
